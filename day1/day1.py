3fa1b90fcd8aba2add509708487d8d37ecdd7f1b
Vedesh <adari.vedesh@gmail.com>
2025-12-13 11:10:13 +0530

    more idiomatic approach and shorter

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,6 @@
-with open("input.txt", "r") as file:
-    data = file.read().strip()
-    calories_str = data.split("\n\n")
-    calories_with_elf = []
-    for snack_strs in calories_str:
-        calories_per_snack = snack_strs.split("\n")
-        sum = 0
-        for calorie_per_snack_str in calories_per_snack:
-            calorie_per_snack_str = int(calorie_per_snack_str)
-            sum += calorie_per_snack_str
-        calories_with_elf.append(sum)
-print(max(calories_with_elf))
+with open("sample.txt", "r") as file:
+    
+    data = file.read().strip().split("\n\n")
+    result = [sum(map(int, i.splitlines())) for i in data]
+
+    print(max(result))
